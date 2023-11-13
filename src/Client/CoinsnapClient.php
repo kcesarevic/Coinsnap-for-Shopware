0bee6966e331727863d508ad4389013c8111d1e5
www-data <nemanja-z@proton.me>
2023-11-13 16:46:33 +0100

    Fix problem with logo prefix

--- src/Client/CoinsnapClient.php
+++ src/Client/CoinsnapClient.php
@@ -18,43 +18,43 @@ use Coinsnap\Shopware\Configuration\ConfigurationService;
 
 class CoinsnapClient extends AbstractClient implements ClientInterface
 {
-  protected ConfigurationService $configurationService;
-  protected LoggerInterface $logger;
+    protected ConfigurationService $configurationService;
+    protected LoggerInterface $logger;
 
-  public function __construct(ConfigurationService $configurationService, LoggerInterface $logger)
-  {
-    $this->configurationService = $configurationService;
+    public function __construct(ConfigurationService $configurationService, LoggerInterface $logger)
+    {
+        $this->configurationService = $configurationService;
 
-    $authorizationHeader = $this->createAuthHeader();
+        $authorizationHeader = $this->createAuthHeader();
 
-    $client = new Client(
-      [
-        'base_uri' => 'https://app.coinsnap.io',
-        'headers' => [
-          'X-Api-Key' => $authorizationHeader
-        ]
+        $client = new Client(
+            [
+            'base_uri' => 'https://app.coinsnap.io',
+            'headers' => [
+              'X-Api-Key' => $authorizationHeader
+            ]
       ]
-    );
-    parent::__construct($client, $logger);
-  }
-  public function sendPostRequest(string $resourceUri, array $data, array $headers = []): array
-  {
-    $headers['content-type'] = 'application/json';
-    $options = [
-      'headers' => $headers,
-      'json'  => $data
-    ];
-    return $this->post($resourceUri, $options);
-  }
-  public function sendGetRequest(string $resourceUri, array $headers = []): array
-  {
-    $options = [
-      'headers' => $headers
-    ];
-    return $this->get($resourceUri, $options);
-  }
-  public function createAuthHeader(): ?string
-  {
-    return $this->configurationService->getSetting('coinsnapApiKey');
-  }
+        );
+        parent::__construct($client, $logger);
+    }
+    public function sendPostRequest(string $resourceUri, array $data, array $headers = []): array
+    {
+        $headers['content-type'] = 'application/json';
+        $options = [
+          'headers' => $headers,
+          'json'  => $data
+        ];
+        return $this->post($resourceUri, $options);
+    }
+    public function sendGetRequest(string $resourceUri, array $headers = []): array
+    {
+        $options = [
+          'headers' => $headers
+        ];
+        return $this->get($resourceUri, $options);
+    }
+    public function createAuthHeader(): ?string
+    {
+        return $this->configurationService->getSetting('coinsnapApiKey');
+    }
 }
